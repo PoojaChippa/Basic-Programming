f308b1470b8e237f97f6a51b508b4349ebffe132
PoojaChippa <poojachippa04@gmail.com>
2025-11-14 23:09:17 +0530

    added EvenorOdd.js

---
+++ EvenOrOdd.js
@@ -0,0 +1,13 @@
+function evenOdd(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let p = evenOdd(4);
+console.log(p);
+
+let q = evenOdd(3);
+console.log(q);
